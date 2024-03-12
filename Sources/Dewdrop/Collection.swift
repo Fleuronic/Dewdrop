0628c82f99506ea21943a79559a5acba96a16430
Jordan Kay <jordan@fleuronic.com>
2024-03-12 19:19:11 -0400

    Add remaining collection properties

--- Sources/Dewdrop/Collection.swift
+++ Sources/Dewdrop/Collection.swift
@@ -2,12 +2,19 @@
 
 import InitMacro
 
+import struct Foundation.Date
 import struct Foundation.URL
 
 @Init public struct Collection {
-	public let title: String
-	public let count: Int
-	public let isPublic: Bool
-	public let isDraggable: Bool
+	public let access: Access
 	public let colorString: String
+	public let coverURL: URL
+	public let count: Int
+	public let creationDate: Date
+	public let isExpanded: Bool
+	public let isPublic: Bool
+	public let sortIndex: Int
+	public let title: String
+	public let updateDate: Date
+	public let view: View
 }
