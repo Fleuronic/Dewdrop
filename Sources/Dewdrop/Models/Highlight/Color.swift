a30b0de9ba3497e65bb25cf16f9c0e1b32fb955a
Jordan Kay <jordan@fleuronic.com>
2024-03-25 09:03:47 -0400

    Add highlight color

---
+++ Sources/Dewdrop/Models/Highlight/Color.swift
@@ -0,0 +1,18 @@
+// Copyright © Fleuronic LLC. All rights reserved.
+
+public extension Highlight {
+	enum Color {
+		case blue
+		case brown
+		case cyan
+		case gray
+		case green
+		case indigo
+		case orange
+		case pink
+		case purple
+		case red
+		case teal
+		case yellow
+	}
+}
