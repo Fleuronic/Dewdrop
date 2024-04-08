bb92b42708d3e57aa08d08e669945608f6cb5123
Jordan Kay <jordan@fleuronic.com>
2024-04-08 10:04:31 -0400

    Update models

--- Sources/Dewdrop/Models/Raindrop/ItemType.swift
+++ Sources/Dewdrop/Models/Raindrop/ItemType.swift
@@ -1,12 +1,10 @@
 // Copyright © Fleuronic LLC. All rights reserved.
 
-public extension Raindrop {
-	enum ItemType {
-		case link 
-		case article 
-		case image 
-		case video 
-		case document
-		case audio
-	}
+public enum ItemType {
+	case link 
+	case article 
+	case image 
+	case video 
+	case document
+	case audio
 }
