bbabad04b52a8c3c1bc3f96fb20bcef3bb676cc9
Jordan Kay <jordan@fleuronic.com>
2024-04-25 17:31:54 -0400

    Update dependencies

---
+++ Sources/Dewdrop/User/Network/Provider.swift
@@ -0,0 +1,12 @@
+// Copyright © Fleuronic LLC. All rights reserved.
+
+public extension Network {
+	enum Provider: String {
+		case facebook
+		case twitter
+		case vkontakte
+		case google
+		case dropbox
+		case gdrive
+	}
+}
--- Sources/Dewdrop/Utilities/Value.swift
+++
@@ -1,7 +0,0 @@
-// Copyright © Fleuronic LLC. All rights reserved.
-
-public extension Swift.Collection {
-	var filledValue: Self? {
-		isEmpty ? nil : self
-	}
-}
