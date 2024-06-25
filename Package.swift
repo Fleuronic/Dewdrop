7aae887e0c823d81956cb299e95035cad794ae88
Jordan Kay <jordan@fleuronic.com>
2024-06-24 23:50:05 -0700

    Use Swift 5.10 for now

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:6.0
+// swift-tools-version:5.10
 import PackageDescription
 
 let package = Package(
@@ -21,6 +21,5 @@ let package = Package(
 			name: "Dewdrop",
 			dependencies: ["InitMacro"]
 		)
-	],
-	swiftLanguageVersions: [.v6]
+	]
 )
