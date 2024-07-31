1fc3cfe9f982a63dd24029fd1a0684b0202ddb93
Jordan Kay <jordan@fleuronic.com>
2024-07-30 20:41:47 -0400

    Update to Swift 6

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.10
+// swift-tools-version:6.0
 import PackageDescription
 
 let package = Package(
@@ -21,5 +21,6 @@ let package = Package(
 			name: "Dewdrop",
 			dependencies: ["InitMacro"]
 		)
-	]
+	],
+	swiftLanguageVersions: [.v6]
 )
