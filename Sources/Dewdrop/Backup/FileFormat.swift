1339874256895466cfbbc4a6b79f5beb9cc33925
Jordan Kay <jordan@fleuronic.com>
2025-01-09 12:20:50 -0500

    Update models

--- Sources/Dewdrop/Backup/FileFormat.swift
+++ Sources/Dewdrop/Backup/FileFormat.swift
@@ -1,6 +1,13 @@
-// Copyright © Fleuronic LLC. All rights reserved.
+//
+//  File.swift
+//  Dewdrop
+//
+//  Created by Jordan Kay on 1/6/25.
+//
 
-public enum FileFormat: String {
-	case html
-	case csv
+public extension Backup {
+	enum FileFormat: String {
+		case html
+		case csv
+	}
 }
---
+++ Sources/Dewdrop/Raindrop/Format.swift
@@ -0,0 +1,9 @@
+// Copyright © Fleuronic LLC. All rights reserved.
+
+public extension Raindrop {
+	enum Format: String, Sendable {
+		case html = "export.html"
+		case csv = "export.csv"
+		case zip = "export.zip"
+	}
+}
