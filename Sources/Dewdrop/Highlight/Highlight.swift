bc08eca5facb99a3243e6f1385d1920f87c3f2aa
Jordan Kay <jordan@fleuronic.com>
2025-02-04 10:08:01 -0500

    Update model initializers

--- Sources/Dewdrop/Highlight/Highlight.swift
+++ Sources/Dewdrop/Highlight/Highlight.swift
@@ -4,10 +4,37 @@ import MemberwiseInit
 
 import struct Foundation.Date
 
-@MemberwiseInit(.public)
+@dynamicMemberLookup
 public struct Highlight: Sendable {
-	public let content: Content
 	public let title: String?
 	public let creationDate: Date
 	public let updateDate: Date
+
+	private let content: Content
+
+	public init(
+		text: String,
+		color: Color = .yellow,
+		note: String? = nil,
+		title: String? = nil,
+		creationDate: Date,
+		updateDate: Date
+	) {
+		self.title = title
+		self.creationDate = creationDate
+		self.updateDate = updateDate
+
+		content = .init(
+			text: text,
+			color: color,
+			note: note
+		)
+	}
+}
+
+// MARK: -
+public extension Highlight {
+	subscript<T>(dynamicMember keyPath: KeyPath<Highlight.Content, T>) -> T {
+		content[keyPath: keyPath]
+	}
 }
--- Sources/Dewdrop/Raindrop/Raindrop.swift
+++ Sources/Dewdrop/Raindrop/Raindrop.swift
@@ -5,12 +5,10 @@ import MemberwiseInit
 import struct Foundation.URL
 import struct Foundation.Date
 
-@MemberwiseInit(.public)
 @dynamicMemberLookup
 public struct Raindrop: Equatable, Sendable {
 	public let url: URL
 	public let domain: String
-	public let info: Info
 	public let note: String?
 	public let media: [Media]
 	public let cache: Cache?
@@ -19,6 +17,43 @@ public struct Raindrop: Equatable, Sendable {
 	public let isBroken: Bool
 	public let creationDate: Date
 	public let updateDate: Date
+
+	private let info: Info
+
+	public init(
+		url: URL,
+		domain: String,
+		title: String,
+		itemType: ItemType = .link,
+		excerpt: String? = nil,
+		coverURL: URL? = nil,
+		note: String? = nil,
+		media: [Media] = [],
+		cache: Cache? = nil,
+		reminder: Reminder? = nil,
+		isFavorite: Bool = false,
+		isBroken: Bool = false,
+		creationDate: Date,
+		updateDate: Date
+	) {
+		self.url = url
+		self.domain = domain
+		self.note = note
+		self.media = media
+		self.cache = cache
+		self.reminder = reminder
+		self.isFavorite = isFavorite
+		self.isBroken = isBroken
+		self.creationDate = creationDate
+		self.updateDate = updateDate
+
+		info = .init(
+			title: title,
+			itemType: itemType,
+			excerpt: excerpt,
+			coverURL: coverURL
+		)
+	}
 }
 
 // MARK: -
